97ede95a1a6128b4bedff96265967f6f242595e0
jadosh123 <jadosh2000@gmail.com>
2021-10-25 21:12:54 +0300

    Removes the console log function

--- src/components/LecturePage/index.jsx
+++ src/components/LecturePage/index.jsx
@@ -47,10 +47,6 @@ export default (props) => {
         setIsOpen(true);
     }
 
-    function whateverWhatever() {
-        console.log("test");
-    }
-
     function afterOpenModal() {
         // references are now sync'd and can be accessed.
         //subtitle.style.color = '#f00';
